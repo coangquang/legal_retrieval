f5399006c189afe4bed30ca500bb854c71e13ae1
admin <nhatquang2306@gmail.com>
2023-10-22 01:15:05 +0700

    Update system

--- retrieval_system.py
+++ retrieval_system.py
@@ -3,9 +3,10 @@ import time
 import json
 import torch
 import pandas as pd
-import faiss
-from datasets import load_dataset
-from dpr.model import BiEncoder
+#import faiss
+#from datasets import load_dataset
+#from dpr.model import BiEncoder
+import argparse
 from dpr.util import get_tokenizer
 from dpr.preprocess import tokenise, preprocess_question
 from pyvi.ViTokenizer import tokenize
@@ -144,3 +145,34 @@ class Retriever():
         all_acc = all_count/len(df)
         hit_acc = hit_count/len(df)
         return hit_acc, all_acc
+    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--scorpus_file", default=None, type=str,
+                        help="Directory of the subcorpus file (scorpus.json)")
+    parser.add_argument("--data_dir", default=None, type=str,
+                        help="Directory of the data folder (containing train, test, val, ttrain, ttest, tval file)")
+    parser.add_argument("--BE_checkpoint", default="vinai/phobert-base-v2", type=str,
+                        help="Name or directory of pretrained model for encoder")
+    parser.add_argument("--BE_representation", default=0, type=int,
+                        help="Type of encoder representation (-10 for avg, -100 for pooled-output)")
+    parser.add_argument("--BE_score", default="dot", type=str,
+                        help="Type of similarity score")
+    parser.add_argument("--q_fixed", default=False, type=bool,
+                        help="To fix question encoder during training stage or not")
+    parser.add_argument("--ctx_fixed", default=False, type=bool,
+                        help="To fix context encoder during training stage or not")
+    parser.add_argument("--q_len", default=32, type=int,
+                        help="Maximum token length for question")
+    parser.add_argument("--ctx_len", default=256, type=int,
+                        help="Maximum token length for context")
+    parser.add_argument("--biencoder_path", default=None, type=str,
+                        help="Path to save the state with highest validation result.")
+    parser.add_argument("--index_path", default=None, type=str,
+                        help="Path to save the index")
+    parser.add_argument("--cross_checkpoint", default="vinai/phobert-base-v2", type=str,
+                        help="Name or directory of pretrained model for cross-encoder")
+    
+    args = parser.parse_args()
+    dpr_retriever = Retriever(args)
+    dpr_retriever.test_on_data(top_k = [1,5,10,30,100])
